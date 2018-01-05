587a803271f2978a3a7b111fdcb6f2f62371eb5e
Wojciech Trocki <wtrocki@redhat.com>
2018-01-05 16:07:01 +0000

    Add namespace

--- installer/roles/ansible-service-broker-setup/files/provision-ansible-service-broker.sh
+++ installer/roles/ansible-service-broker-setup/files/provision-ansible-service-broker.sh
@@ -53,7 +53,7 @@ oc process -f "${TEMPLATE_LOCAL}" \
 -p ETCD_TRUSTED_CA="$ETCD_CA_CERT" \
 -p BROKER_CLIENT_CERT="$BROKER_CLIENT_CERT" \
 -p BROKER_CLIENT_KEY="$BROKER_CLIENT_KEY" \
--p NAMESPACE=ansible-service-broker 
+-p NAMESPACE=ansible-service-broker \
 -p LAUNCH_APB_ON_BIND="${LAUNCH_APB_ON_BIND}" \
 ${TEMPLATE_VARS} | oc create -f -
 
